6013a10be4f37879affb52f84350e1ce862e2d35
Aishwarya <aishwarya92@berkeley.edu>
2015-09-23 11:16:35 -0700

    completed hw 2

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -11,6 +11,23 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet weak var spaceButton: UIButton!
+    @IBOutlet weak var retButton: UIButton!
+    @IBOutlet weak var delButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var helloButton: UIButton!
+    @IBOutlet weak var byeButton: UIButton!
     
     var keyboardView: UIView!
 
@@ -46,6 +63,46 @@ class KeyboardViewController: UIInputViewController {
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
     }
-
-
+    
+    @IBAction func spaceButtonPressed(sender: UIButton) {
+        self.textDocumentProxy.insertText(" ")
+        
+    }
+    
+    @IBAction func retButtonPressed(sender: UIButton) {
+        self.textDocumentProxy.insertText("\n")
+    }
+   
+    @IBAction func delButtonPressed(sender: UIButton) {
+        self.textDocumentProxy.deleteBackward()
+    }
+    @IBAction func likeButtonPressed(sender: UIButton) {
+        self.textDocumentProxy.insertText("👍")
+    }
+    @IBAction func dislikeButtonPressed(sender: AnyObject) {
+        self.textDocumentProxy.insertText("👎")
+    }
+    @IBAction func numberButtonPressed(sender: UIButton) {
+        switch sender.tag
+        {
+            case 0: self.textDocumentProxy.insertText("0")
+            case 1: self.textDocumentProxy.insertText("1")
+            case 2: self.textDocumentProxy.insertText("2")
+            case 3: self.textDocumentProxy.insertText("3")
+            case 4: self.textDocumentProxy.insertText("4")
+            case 5: self.textDocumentProxy.insertText("5")
+            case 6: self.textDocumentProxy.insertText("6")
+            case 7: self.textDocumentProxy.insertText("7")
+            case 8: self.textDocumentProxy.insertText("8")
+            case 9: self.textDocumentProxy.insertText("9")
+        default: self.textDocumentProxy.insertText(" ")
+        
+        }
+    }
+    @IBAction func helloButtonPressed(sender: AnyObject) {
+        self.textDocumentProxy.insertText("Hello")
+    }
+    @IBAction func byeButtonPressed(sender: AnyObject) {
+        self.textDocumentProxy.insertText("Bye")
+    }
 }
